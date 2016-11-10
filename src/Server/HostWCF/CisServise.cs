4bca73d0c1a2fb7231a88ff9df662b8ab9fb0267
Aldmi <Aldmi@bk.ru>
2016-11-10 20:45:50 +0700

    Test data for WCF service

--- src/Server/HostWCF/CisServise.cs
+++ src/Server/HostWCF/CisServise.cs
@@ -84,71 +84,98 @@ namespace Server.HostWCF
             //          {
             //              Task.Factory.StartNew(action);
             //          });
-            //DEBUG-----------------------------------------------------
-            try
+
+
+
+            return new List<OperativeScheduleData>
             {
-                var query = _unitOfWork.RailwayStationRepository.Search(r => r.Name == nameRailwayStation, null, "OperativeSchedules").AsNoTracking();
-                var railwayStation = await query.FirstOrDefaultAsync();
-                if (railwayStation == null)
+                new OperativeScheduleData
                 {
-                    var ex= new ArgumentNullException($"{nameRailwayStation}");
-                    throw new FaultException<ArgumentNullException>(ex, $"Вокзал с таким именем не найден \"{nameRailwayStation}\"");
+                    ArrivalTime = DateTime.Now,
+                    DepartureTime = DateTime.Today,
+                    NumberOfTrain = 10,
+                    Id = 1,
+                    RouteName = "Маршрут 1",
+                    DispatchStation = new StationsData {Id = 1, Description = "описание 1", Name = "станция 1", EcpCode = 10},
+                    StationOfDestination = new StationsData {Id = 3, Description = "описание 3", Name = "станция 3", EcpCode = 56}
+                },
+                new OperativeScheduleData
+                {
+                    ArrivalTime = DateTime.Now,
+                    DepartureTime = DateTime.Today,
+                    NumberOfTrain = 25,
+                    Id = 2,
+                    RouteName = "Маршрут 2",
+                    DispatchStation = new StationsData {Id = 2, Description = "описание 2", Name = "станция 2", EcpCode = 23},
+                    StationOfDestination = new StationsData {Id = 45, Description = "описание 45", Name = "станция 45", EcpCode = 569}
                 }
+            };
+            //DEBUG-----------------------------------------------------
 
-                var operativeSchedules = (count != null) ? railwayStation.OperativeSchedules.Take(count.Value) : railwayStation.OperativeSchedules;
+            //try
+            //{
+            //    var query = _unitOfWork.RailwayStationRepository.Search(r => r.Name == nameRailwayStation, null, "OperativeSchedules").AsNoTracking();
+            //    var railwayStation = await query.FirstOrDefaultAsync();
+            //    if (railwayStation == null)
+            //    {
+            //        var ex = new ArgumentNullException($"{nameRailwayStation}");
+            //        throw new FaultException<ArgumentNullException>(ex, $"Вокзал с таким именем не найден \"{nameRailwayStation}\"");
+            //    }
 
-                return
-                    operativeSchedules.Select(op => new OperativeScheduleData
-                    {
-                        Id = op.Id,
-                        RouteName = op.RouteName,
-                        ArrivalTime = op.ArrivalTime,
-                        DepartureTime = op.DepartureTime,
-                        NumberOfTrain = op.NumberOfTrain,
-                        DispatchStation =
-                            new StationsData
-                            {
-                                Id = op.DispatchStation.Id,
-                                Name = op.DispatchStation.Name,
-                                Description = op.DispatchStation.Description,
-                                EcpCode = op.DispatchStation.EcpCode
-                            },
-                        StationOfDestination =
-                            new StationsData
-                            {
-                                Id = op.StationOfDestination.Id,
-                                Name = op.StationOfDestination.Name,
-                                Description = op.StationOfDestination.Description,
-                                EcpCode = op.StationOfDestination.EcpCode
-                            },
-                        ListOfStops =
-                            new List<StationsData>(
-                                op.ListOfStops.Select(
-                                    st =>
-                                        new StationsData
-                                        {
-                                            Id = st.Id,
-                                            Name = st.Name,
-                                            EcpCode = st.EcpCode,
-                                            Description = st.Description
-                                        })),
-                        ListWithoutStops =
-                            new List<StationsData>(
-                                op.ListWithoutStops.Select(
-                                    st =>
-                                        new StationsData
-                                        {
-                                            Id = st.Id,
-                                            Name = st.Name,
-                                            EcpCode = st.EcpCode,
-                                            Description = st.Description
-                                        }))
-                    }).ToList();
-            }
-            catch (Exception ex)
-            {
-                throw new FaultException<Exception>(ex, $"Запрос для вокзала\"{nameRailwayStation}\" привел к ошибке на ЦИС сервере");
-            }
+            //    var operativeSchedules = (count != null) ? railwayStation.OperativeSchedules.Take(count.Value) : railwayStation.OperativeSchedules;
+
+            //    return
+            //        operativeSchedules.Select(op => new OperativeScheduleData
+            //        {
+            //            Id = op.Id,
+            //            RouteName = op.RouteName,
+            //            ArrivalTime = op.ArrivalTime,
+            //            DepartureTime = op.DepartureTime,
+            //            NumberOfTrain = op.NumberOfTrain,
+            //            DispatchStation =
+            //                new StationsData
+            //                {
+            //                    Id = op.DispatchStation.Id,
+            //                    Name = op.DispatchStation.Name,
+            //                    Description = op.DispatchStation.Description,
+            //                    EcpCode = op.DispatchStation.EcpCode
+            //                },
+            //            StationOfDestination =
+            //                new StationsData
+            //                {
+            //                    Id = op.StationOfDestination.Id,
+            //                    Name = op.StationOfDestination.Name,
+            //                    Description = op.StationOfDestination.Description,
+            //                    EcpCode = op.StationOfDestination.EcpCode
+            //                },
+            //            ListOfStops =
+            //                new List<StationsData>(
+            //                    op.ListOfStops.Select(
+            //                        st =>
+            //                            new StationsData
+            //                            {
+            //                                Id = st.Id,
+            //                                Name = st.Name,
+            //                                EcpCode = st.EcpCode,
+            //                                Description = st.Description
+            //                            })),
+            //            ListWithoutStops =
+            //                new List<StationsData>(
+            //                    op.ListWithoutStops.Select(
+            //                        st =>
+            //                            new StationsData
+            //                            {
+            //                                Id = st.Id,
+            //                                Name = st.Name,
+            //                                EcpCode = st.EcpCode,
+            //                                Description = st.Description
+            //                            }))
+            //        }).ToList();
+            //}
+            //catch (Exception ex)
+            //{
+            //    throw new FaultException<Exception>(ex, $"Запрос для вокзала\"{nameRailwayStation}\" привел к ошибке на ЦИС сервере");
+            //}
         }
 
 
